c4a95509286c19a665e15d8110e19082a303a623
alexandru-andoni <aaa_alexz@yahoo.com>
2026-03-13 15:00:25 +0200

    Clean up irrelevant comments in org.adonis.editor.java
    Removed irrelevant sample code comments from the file.

--- class org.adonis.editor.java
+++ class org.adonis.editor.java
@@ -1,6 +1,8 @@
 //literally just ignore this code
 //it was a sample code that i tried to compile using the editor
+//and it somehow infiltrated into the project
 //no relevancy whatsoever with the actual thing
+//ig that's what i get for fucking around
 //cheers
 //al
 
